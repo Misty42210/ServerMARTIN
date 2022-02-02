48ff4200cca34280e38aca96abe996a10f14aa65
Misty42210 <francois.martin@etu.univ-st-etienne.fr>
2022-02-02 15:55:01 +0100

    renvoie

--- src/Controller/MARTINController.php
+++ src/Controller/MARTINController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class MARTINController extends AbstractController
 {
@@ -15,6 +16,23 @@ class MARTINController extends AbstractController
     {
         return $this->render('martin/index.html.twig', [
             'controller_name' => 'MARTINController',
+        ]);
+    }
+    /**
+     * @Route("/srv/login", name="login")
+     */
+    public function login(Request $request): Response
+    {
+        $login = $request->get("pseudo");
+        $password = $request->request->get("pass");
+        if ($login=="root" && $password=="toor")
+            $message = "vous avez reussi a vous connecter";
+        else
+        $message = "pas le bon identifiant ou mot de passe";
+        return $this->render('martin/login.html.twig', [
+            'login' =>$login,
+            'password' =>$password,
+            'message'=> $message,
         ]);
     }
 }
